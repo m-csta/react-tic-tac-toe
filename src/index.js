42f778f8bede85dced112f13949d7a0a2780c822
m-csta <90424687+m-csta@users.noreply.github.com>
2021-11-26 22:44:52 -0500

    add helper function

--- src/index.js
+++ src/index.js
@@ -73,6 +73,17 @@ function Square(props) {
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+         );
+      });
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -89,8 +100,8 @@ function Square(props) {
           />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
@@ -103,4 +114,24 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
