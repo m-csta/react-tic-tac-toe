1fb1a2af78dde967c1a20e23dfa610af57ba2d2d
m-csta <90424687+m-csta@users.noreply.github.com>
2021-11-26 22:28:20 -0500

    reaplce square class with function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-        className="square" 
-        onClick={() => this.props.onClick({value: 'X'})}>
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
   
   class Board extends React.Component {
     constructor(props) {
@@ -20,6 +16,11 @@ class Square extends React.Component {
         this.state = {
             squares: Array(9).fill(null),
         };  
+    }
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
     }
     renderSquare(i) {
         return 
